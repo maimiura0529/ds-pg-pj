35c94cd681db6e1bd4e6465268cf7c43ac0bedf5
Mai Miura <s2322059@stu.musashino-u.ac.jp>
2024-01-12 02:35:26 +0900

    最初からやり直します。データベースファイルを作成し、中にテーブルを作成。

--- main_code.py
+++ main_code.py
@@ -18,28 +18,7 @@ db_path = os.path.join(path, db_name)
 # DBに接続する（指定したDBファイルが存在しない場合は、新規に作成される）
 con = sqlite3.connect(db_path)
 
-# ここでデータベースの操作を行う（例：テーブル作成、データの挿入など）
-
 # DBへの接続を閉じる
 con.close()
 
 #-------------------------------------------------------#
-
-# DBに接続する
-con = sqlite3.connect(path + db_name)
-# print(type(con))
-
-# SQLを実行するためのオブジェクトを取得
-cur = con.cursor()
-
-# 実行したいSQLを用意する
-# テーブルを作成するSQL
-# CREATE TABLE テーブル名（カラム名 型，...）;
-sql_create_table_temperature_data = 'CREATE TABLE temperature_data(temperature_ave real, humidity_ave real, sunshine_hours real);'
-
-# 4．SQLを実行する
-cur.execute(sql_create_table_temperature_data)
-
-# 6．DBへの接続を閉じる
-con.close()
-
